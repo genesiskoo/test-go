66cd878f2b3193b9612c9059c62a30f487a1ac6f
jaykoo <genesiskoo@gmail.com>
2021-06-01 18:21:38 +0900

    end of practice

--- main.go
+++ main.go
@@ -14,9 +14,44 @@ func main() {
 	// result := supperAdd(3, 2, 5, 2, 5, 6)
 	// fmt.Println(result)
 
-	result := canIDrink(15)
-	fmt.Println(result)
+	// result := canIDrink(15)
+	// fmt.Println(result)
 
+	// POINTER
+	// a := 2
+	// b := &a
+	// c := &a
+	// fmt.Println(a, *b, c)
+
+	//&포인터 지정, 메모리값
+	//포인터 지정시 해당 메모리를 값으로 가짐
+
+	// & address
+	// * see though
+
+	// ARRAY & SLICE
+	// name := []string{"one", "two", "three"}
+	// name = append(name, "four")
+
+	// fmt.Println(name)
+
+	// MAP
+	// koo := map[string]string{"name": "koo", "age": "15"}
+	// fmt.Println(koo)
+	// for key, value := range koo {
+	// 	fmt.Println(key, value)
+	// }
+
+	favFood := []string{"kim", "chi"}
+	koo := person{name: "koo", age: 21, favFood: favFood}
+	fmt.Println(koo)
+
+}
+
+type person struct {
+	name    string
+	age     int
+	favFood []string
 }
 
 func repeatMe(words ...string) {
@@ -33,6 +68,7 @@ func canIDrink(age int) bool {
 		return false
 	}
 	return true
+
 }
 
 func supperAdd(nums ...int) int {
